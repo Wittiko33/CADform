9163818f2cfff725df16f10757947466849aa795
Wittiko33 <wittiko@proton.me>
2024-03-13 19:39:53 +0100

    Add call to create a wall anchor in __name__ guard
    - also fixes not NoneType error in WallAnchors super()._init__
    - TODO: add parameter optional for corner radius
    - TODO: add parameter for plate thickness

--- wall-anchor.py
+++ wall-anchor.py
@@ -11,12 +11,14 @@ class WallAnchor(Compound):
     """A basic adhesive wall anchor for keyhole slots"""
     def __init__(self, screw_diameter: int, screw_length:int, glue_area:int):
         with BuildPart() as bp:
-            with BuildSketch:
+            with BuildSketch():
                 RectangleRounded(glue_area, glue_area, 2)
+            extrude(amount=2)
         super().__init__(bp.part.wrapped)
 
 
 
 
 if __name__=="__main__":
-    pass
+    my_anchor = WallAnchor(screw_diameter=SCREW_DIAMETER, screw_length=SCREW_LENGTH, glue_area=GLUE_AREA_LENGTH)
+    show(my_anchor)
