3a341a299f828cddc12195c31fad9f870f239267
Jan Hautmann <jan.hautmann@proton.me>
2024-03-13 19:43:02 +0100

    Add plate_thickness and corner_radius parameters

--- wall-anchor.py
+++ wall-anchor.py
@@ -9,16 +9,16 @@ GLUE_AREA_LENGTH = 25
 
 class WallAnchor(Compound):
     """A basic adhesive wall anchor for keyhole slots"""
-    def __init__(self, screw_diameter: int, screw_length:int, glue_area:int):
+    def __init__(self, screw_diameter: int, screw_length:int, glue_area:int, plate_thickness:int, corner_radius:int=2):
         with BuildPart() as bp:
             with BuildSketch():
-                RectangleRounded(glue_area, glue_area, 2)
-            extrude(amount=2)
+                RectangleRounded(glue_area, glue_area, corner_radius)
+            extrude(amount=plate_thickness)
         super().__init__(bp.part.wrapped)
 
 
 
 
 if __name__=="__main__":
-    my_anchor = WallAnchor(screw_diameter=SCREW_DIAMETER, screw_length=SCREW_LENGTH, glue_area=GLUE_AREA_LENGTH)
+    my_anchor = WallAnchor(screw_diameter=SCREW_DIAMETER, screw_length=SCREW_LENGTH, glue_area=GLUE_AREA_LENGTH, plate_thickness=2)
     show(my_anchor)
